4352b0948022e1238f442f3c810ca450d5dbee65
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-02-18 20:47:52 +0900

    join

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const express=require('express');
 const app=express();
 const path = require("path");
+const qs = require('querystring');
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'page')));
 
 const db=require('./lib/db');
@@ -12,6 +15,53 @@ app.get('/', function(request, response){
 
 app.get('/join', function(request, response){
     response.sendFile(path.join(__dirname, 'page', 'join.html'));
-})
+});
+
+app.post('/join_process', function(request, response){
+    let name=request.body.name;
+    let email=request.body.email;
+    let password=request.body.password;
+    db.query(`INSERT INTO user (nickname, email, password)
+    VALUES (?,?,?)`, [name, email, password], function(error, result){
+        if(error){
+            throw error;
+        }
+        response.redirect('/');
+    });
+});
+
+app.post('/login_process', function(request, response){
+    let email=request.body.email;
+    let password=request.body.password;
+    db.query('SELECT * FROM user WHERE email = ?', [email],
+    function( error, results, fields) {
+        if (error) {
+            response.send({
+                "code": 400,
+                "failed": "error ocurred"
+            })
+        } else {
+            console.log(results);
+            if(results.length > 0) {    //그 이메일로 된 pw가 있다는 것
+                if(results[0].password == password) {
+                    response.send({
+                        "code": 200,
+                        "success": "login sucessfull"
+                    });
+                } else {
+                    response.send({
+                        "code": 204,
+                        "success": "Email and password does not match"
+                    });
+                }
+            } else {
+                response.send({
+                    "code":204,
+                    "success": "Email does not exists"
+                });
+            }
+        }    
+    });
+});
 
 app.listen(3000);
