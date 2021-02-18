b079041aa014abc5e065b155e53d0a56d1c39c3a
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-02-18 20:16:11 +0900

    join

--- main.js
+++ main.js
@@ -2,12 +2,16 @@ const express=require('express');
 const app=express();
 const path = require("path");
 
-app.use(express.static('page'));
+app.use(express.static(path.join(__dirname, 'page')));
 
 const db=require('./lib/db');
 
 app.get('/', function(request, response){
-    response.sendFile('index.html');
+    response.sendFile(path.join(__dirname, 'page', 'index.html'));
 });
+
+app.get('/join', function(request, response){
+    response.sendFile(path.join(__dirname, 'page', 'join.html'));
+})
 
 app.listen(3000);
