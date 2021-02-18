8ba236809fcc8f06e4644ec80b35db926fa518ba
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-02-18 21:00:03 +0900

    login

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ const app=express();
 const path = require("path");
 const qs = require('querystring');
 const bodyParser = require('body-parser');
+const user=require('./lib/user');
+const db=require('./lib/db');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'page')));
-
-const db=require('./lib/db');
 
 app.get('/', function(request, response){
     response.sendFile(path.join(__dirname, 'page', 'index.html'));
@@ -31,37 +31,7 @@ app.post('/join_process', function(request, response){
 });
 
 app.post('/login_process', function(request, response){
-    let email=request.body.email;
-    let password=request.body.password;
-    db.query('SELECT * FROM user WHERE email = ?', [email],
-    function( error, results, fields) {
-        if (error) {
-            response.send({
-                "code": 400,
-                "failed": "error ocurred"
-            })
-        } else {
-            console.log(results);
-            if(results.length > 0) {    //그 이메일로 된 pw가 있다는 것
-                if(results[0].password == password) {
-                    response.send({
-                        "code": 200,
-                        "success": "login sucessfull"
-                    });
-                } else {
-                    response.send({
-                        "code": 204,
-                        "success": "Email and password does not match"
-                    });
-                }
-            } else {
-                response.send({
-                    "code":204,
-                    "success": "Email does not exists"
-                });
-            }
-        }    
-    });
+    user.login(request, response);
 });
 
 app.listen(3000);
