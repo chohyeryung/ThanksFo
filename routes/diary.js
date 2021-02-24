bd083f91c0a199b91815d4714eea85b32e7ccdf4
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-02-24 13:14:06 +0900

    diary create~

--- routes/diary.js
+++ routes/diary.js
@@ -1,12 +1,22 @@
 const express=require('express');
 const router=express.Router();
 const app=express();
+const bodyParser = require('body-parser');
+const session=require('express-session');
+const MySQLStore=require('express-mysql-session')(session);
+const cookieParser = require('cookie-parser');
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/views'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 router.get('/create', function(request, response){
-    response.render('create.ejs');
+    response.render('create.ejs', {name : request.session.user.nickname});
+});
+
+router.get('/info', function(request, response){
+    response.render('info.ejs');
 });
 
 module.exports=router;
