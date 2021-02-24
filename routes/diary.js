8b83136a501e40c25b80ef0cdfd8a20f696a929e
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-02-24 12:38:12 +0900

    diary page go!

--- routes/diary.js
+++ routes/diary.js
@@ -1,8 +1,11 @@
 const express=require('express');
 const router=express.Router();
+const app=express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/views'));
 
 router.get('/create', function(request, response){
-
 });
 
 module.exports=router;
