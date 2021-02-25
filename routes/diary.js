91f052a63b1e4ad0a2e2200c2f869347a58fe087
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-02-25 21:38:37 +0900

    diary show_detail !!!!!!!

--- routes/diary.js
+++ routes/diary.js
@@ -6,8 +6,7 @@ const session=require('express-session');
 const MySQLStore=require('express-mysql-session')(session);
 const cookieParser = require('cookie-parser');
 const db = require('../lib/db');
-const template = require('../lib/template');
-const { connect } = require('../lib/db');
+const date_format = require('dateformat');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
@@ -40,32 +39,37 @@ router.post('/create_process', function(request, response){
 });
 
 router.get('/show', function(request, response){
-    const query="SELECT * FROM thdiary WHERE user_id=?";
+    const query1 = "SELECT * FROM thdiary WHERE user_id=?";
+    const query2 = "SELECT * FROM thdiary WHERE created = (SELECT MAX(created) FROM thdiary)";
     let uid=request.session.user.idx;
-    db.query(query, [uid], function(error, diarys){
+    db.query(query1, [uid], function(error, diarys){
         if (error) {
             throw error;
         }
-        response.render('show.ejs', {result : diarys});
+        db.query(query2, function(error2, diary){
+            if (error2) {
+                throw error;
+            }
+            response.render('show.ejs', {result : diary});
+        });
     });
 });
 
 router.post('/show_detail', function(request, response){
-    let created=request.body.created;
-    const query1="SELECT * FROM thdiary WHERE user_id=?";
-    const query2="SELECT * FROM thdiary WHERE created LIKE "+db.escape(created+'%');
+    // const query1="SELECT * FROM thdiary WHERE user_id=?";
+    const query2="SELECT * FROM thdiary";
     let uid=request.session.user.idx;
-    db.query(query1, [uid], function(error, diary){
-        if (error) {
-            throw error;
-        }
+    // db.query(query1, [uid], function(error, diary){
+    //     if (error) {
+    //         throw error;
+    //     }
         db.query(query2, function(error2, detail_diary){
             if (error2) {
                 throw error2;
             }
             response.render('show_detail.ejs', {result : detail_diary});
         });
-    });
+    // });
 })
 
 module.exports=router;
